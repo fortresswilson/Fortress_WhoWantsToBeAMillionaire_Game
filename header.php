fd3ff327165905b57c464170cad09e37dcd059de
fortresswilson <108816788+fortresswilson@users.noreply.github.com>
2026-04-18 09:54:47 -0400

    extra css styling

--- header.php
+++ header.php
@@ -7,17 +7,18 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
   <nav class="main-nav">
     <div class="nav-logo">Who Wants to Be a Millionaire</div>
     <div class="nav-user">
-      <?php if (isset($_SESSION["username"])): ?>
-          <span><?php echo htmlspecialchars($_SESSION["username"]); ?></span>
-          <a href="logout.php">Logout</a>
+      <?php if (isset($_SESSION['username'])): ?>
+        <span><?php echo htmlspecialchars($_SESSION['username']); ?></span>
+        <a href="logout.php">Logout</a>
       <?php else: ?>
-          <a href="login.php">Log In</a>
-          <a href="register.php">Register</a>
+        <a href="login.php">Log In</a>
+        <a href="register.php">Register</a>
       <?php endif; ?>
     </div>
   </nav>
